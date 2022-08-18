e0dd7293723f6bd91358a1eafd1812bb00fc6079
Ana Ordonez <ordonez4@llnl.gov>
2022-08-18 10:52:52 -0700

    add env vars

--- cmec/cori_slurm.sh
+++ cmec/cori_slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "#!/bin/bash -l" >> ${CMEC_WK_DIR}/submit_script.sh
-echo "#SBATCH --job-name=cmec_e3sm_diags" >> ${CMEC_WK_DIR}/submit_script.sh
-echo "#SBATCH --qos=debug" >> ${CMEC_WK_DIR}/submit_script.sh
-echo "#SBATCH --account="${ACCT_CODE} >> ${CMEC_WK_DIR}/submit_script.sh
-echo "#SBATCH --nodes=1" >> ${CMEC_WK_DIR}/submit_script.sh
-echo "#SBATCH --time=00:30:00" >> ${CMEC_WK_DIR}/submit_script.sh
+echo "#SBATCH --job-name="${CMEC_JOB_NAME} >> ${CMEC_WK_DIR}/submit_script.sh
+echo "#SBATCH --qos="${CMEC_QUEUE} >> ${CMEC_WK_DIR}/submit_script.sh
+echo "#SBATCH --account="${CMEC_ACCT_CODE} >> ${CMEC_WK_DIR}/submit_script.sh
+echo "#SBATCH --nodes="${CMEC_NODES} >> ${CMEC_WK_DIR}/submit_script.sh
+echo "#SBATCH --time="${CMEC_TIME} >> ${CMEC_WK_DIR}/submit_script.sh
 echo "#SBATCH -C haswell" >> ${CMEC_WK_DIR}/submit_script.sh
 
 echo "source /global/common/software/e3sm/anaconda_envs/load_latest_e3sm_unified_cori-haswell.sh" >> ${CMEC_WK_DIR}/submit_script.sh
